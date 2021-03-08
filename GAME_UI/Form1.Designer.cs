e02185a6ee81d6df07d21e2e1f1268c3248b1043
DESKTOP-4HE6O4U\Chathu <chathurikawijesooriya97@gmail.com>
2021-03-08 21:54:16 +0530

    first commit

---
+++ GAME_UI/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace GAME_UI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.slid = new System.Windows.Forms.PictureBox();
+            this.vert = new System.Windows.Forms.Timer(this.components);
+            this.horz = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ball
+            // 
+            this.ball.Image = ((System.Drawing.Image)(resources.GetObject("ball.Image")));
+            this.ball.Location = new System.Drawing.Point(12, 12);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(88, 79);
+            this.ball.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ball.TabIndex = 0;
+            this.ball.TabStop = false;
+            this.ball.Click += new System.EventHandler(this.ball_Click);
+            // 
+            // slid
+            // 
+            this.slid.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.slid.BackColor = System.Drawing.Color.Lime;
+            this.slid.Location = new System.Drawing.Point(233, 414);
+            this.slid.Name = "slid";
+            this.slid.Size = new System.Drawing.Size(180, 15);
+            this.slid.TabIndex = 1;
+            this.slid.TabStop = false;
+            // 
+            // vert
+            // 
+            this.vert.Enabled = true;
+            this.vert.Interval = 1;
+            this.vert.Tick += new System.EventHandler(this.vert_Tick);
+            // 
+            // horz
+            // 
+            this.horz.Enabled = true;
+            this.horz.Interval = 1;
+            this.horz.Tick += new System.EventHandler(this.horz_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlText;
+            this.ClientSize = new System.Drawing.Size(917, 429);
+            this.Controls.Add(this.slid);
+            this.Controls.Add(this.ball);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.PictureBox slid;
+        private System.Windows.Forms.Timer vert;
+        private System.Windows.Forms.Timer horz;
+    }
+}
+
---
+++ GAME_UI/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GAME_UI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int vdir = +1;
+        private void vert_Tick(object sender, EventArgs e)
+        {
+            if (ball.Top < 0)
+            {
+                vdir = +1;
+            }
+            else if (ball.Top > (this.Height - ball.Height - slid.Height))
+            {
+                vdir = -1;
+            }
+            else
+            {
+                ball.Top += vdir;
+            }
+
+        }
+
+        int hdir = +1;
+        private void horz_Tick(object sender, EventArgs e)
+        {
+            if (ball.Left < 0)
+            {
+                hdir = +1;
+            }
+            else if (ball.Left > (this.Width - ball.Width))
+            {
+                hdir = -1;
+            }
+            else
+            {
+                ball.Left += hdir;
+            }
+        }
+
+        private void ball_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
